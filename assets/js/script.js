2cba89306e9ddc68a2b2e73977c310f6767634eb
NileshPatel83 <hselin_83@yahoo.com>
2023-01-13 14:32:09 +1000

    processProjectData method added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,53 @@
+const keyText = 'project-tracker';
+
 let dateInputEl = $('#datepicker');
-let addProjectEl = $('#add-project');
+let addProjectFormEl = $('#add-project-form')
+let projNameEl = $('#project-name');
+let projTypeEl = $('#project-type');
 let projDisplayEl = $('#project-display');
 
-// function displayProjectForm(){
+function getNewProectData(){
 
-// }
+    let projName = projNameEl.val();
+    let projType = projTypeEl.val();
+    let dueDate = dateInputEl.val();
 
-// addButtonEl.on('submit', displayProjectForm());
+    if (!projName || !projType || !dueDate) return null;
 
-// Datepicker widget
-$(function () {
-    $('#datepicker').datepicker({
-      changeMonth: true,
-      changeYear: true,
-    });
-  });
+    let projectData = {
+        projectName : projName,
+        projectType : projType,
+        dueDate : dueDate
+    };
+
+    return projectData;
+}
+
+function processProjectData (event){
+
+    event.preventDefault();
+    
+    //Gets the project data.
+    let projectData = getNewProectData();
+    if(projectData == null) return;
+
+    //Displays project data in table.
+    displayProjectData();
+
+    //Get project data counter for local storage.
+
+    //Store project data to local storage.
+}
+
+addProjectFormEl.on('submit', processProjectData);
+
+// // Datepicker widget
+// $(function () {
+//     dateInputEl.datepicker({
+//       changeMonth: true,
+//       changeYear: true,
+//     });
+//   });
 
 var today = dayjs();
 $('#current-day').text(today.format('MMM D, YYYY'));
