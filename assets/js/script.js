46dc85bfabd391be9cbbacf5071df3acbe77d121
NileshPatel83 <hselin_83@yahoo.com>
2023-01-13 13:07:27 +1000

    New project modal form added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
+let addButtonEl = $('#add-button');
+let containerEl = $('#container');
+
+// function displayProjectForm(){
+
+// }
+
+// addButtonEl.on('submit', displayProjectForm());
+
 var today = dayjs();
 $('#current-day').text(today.format('MMM D, YYYY'));
 
 let timeInterval = setInterval(function () {
     $('#current-time').text(dayjs().format('h:mm:ss A'));
 }, 1000);  
-
